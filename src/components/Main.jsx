34f44cb05b77926d72fbb42daf37450f8ab35f18
Emrah Ozdemir <info@emrahozdemir.com>
2020-10-01 02:33:44 +0300

    bootstrap 4 added

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+
+export default class Main extends Component {
+    render() {
+    return (
+        <Container as="main" role="main">
+            <div className="starter-template">
+                <h1>Bootstrap starter template</h1>
+                
+                    Use this document as a way to quickly start any new project.<br />
+                    All you get is this text and a mostly barebones HTML document.
+              
+            </div>
+        </Container>
+    )
+    }
+}
