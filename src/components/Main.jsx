0cd7cdf7bd0529702a625a8d76786afefcea8478
Emrah Ozdemir <info@emrahozdemir.com>
2020-10-01 14:35:20 +0300

    404 page added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { Home, Login, Profile, Chat } from '../pages';
+import { Home, Login, Profile, Chat, Page404 } from '../pages';
 
 export default class Main extends Component {
     render() {
@@ -15,6 +15,7 @@ export default class Main extends Component {
                         <Route exact path="/login" component={Login} />
                         <Route exact path="/profile" component={Profile} />
                         <Route exact path="/chat" component={Chat} />
+                        <Route path="/*" component={Page404} />
                     </Switch>
                 </BrowserRouter>
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Home } from './Home';
 export { default as Login } from './Login';
 export { default as Chat } from './Chat';
 export { default as Profile } from './Profile';
+export { default as Page404 } from './Page404';
